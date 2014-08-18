b70551f4297cc1b9415f1526810f9465d7db7c7c
max <mk@kernel-corp.com>
2014-08-18 17:19:38 +0700

    redirect after save

--- lib/generators/ordinary_cms/install/install_generator.rb
+++ lib/generators/ordinary_cms/install/install_generator.rb
@@ -2,9 +2,21 @@ require 'rails/generators'
 
 module OrdinaryCms
   class InstallGenerator < Rails::Generators::Base
+    source_root OrdinaryCms::Engine.root
     argument :name, type: :string, default: "AdminUser"
+
     def generate_activeadmin
       generate("active_admin:install", name) unless ARGV.include? "--skip-activeadmin"
     end
+
+    def copy_mercury_config
+      copy_file 'app/assets/javascripts/mercury.js'
+    end
+
+    def add_routes
+      route %Q{mount Mercury::Engine => '/'}
+      route %Q{mount OrdinaryCms::Engine => '/'}
+    end
+
   end
 end
