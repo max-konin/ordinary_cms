5bb516d3940d314837f214f2b8f72146ea4f6c5b
max <mk@kernel-corp.com>
2014-09-25 13:58:21 +0700

    resolve namespacing

---
+++ app/models/ordinary_cms/mercury/image.rb
@@ -0,0 +1,21 @@
+module OrdinaryCms
+  module Mercury
+    class Image
+      include Mongoid::Document
+      include Mongoid::Paperclip
+
+      has_mongoid_attached_file :image
+
+      validates_presence_of :image
+
+      delegate :url, :to => :image
+
+      def serializable_hash(options = nil)
+        options ||= {}
+        options[:methods] ||= []
+        options[:methods] << :url
+        super(options)
+      end
+    end
+  end
+end
