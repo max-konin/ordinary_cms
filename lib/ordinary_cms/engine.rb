f79116fe573c97e6ff996a462abd8b31484a820f
max <mk@kernel-corp.com>
2014-09-30 16:51:37 +0700

    requires

--- lib/ordinary_cms/engine.rb
+++ lib/ordinary_cms/engine.rb
@@ -1,6 +1,8 @@
 require 'acts_as_page'
 require 'active_admin'
 require 'active_admin-mongoid'
+require 'mongoid_orderable'
+require 'active_admin-sortable'
 
 module OrdinaryCms
   class Engine < ::Rails::Engine
