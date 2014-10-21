6a9080c3a8dac566110b687438dabd76d5aa8925
max <mk@kernel-corp.com>
2014-10-21 13:02:15 +0700

    fix gemfile

--- lib/ordinary_cms/engine.rb
+++ lib/ordinary_cms/engine.rb
@@ -2,6 +2,7 @@ require 'acts_as_page'
 require 'active_admin'
 require 'active_admin-mongoid'
 require 'mongoid_orderable'
+require 'mongoid_paperclip'
 require 'activeadmin-sortable'
 
 module OrdinaryCms
